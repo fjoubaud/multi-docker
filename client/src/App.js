76e5f29ee0daf1352476d729cdcefe67a855a7b7
Jeyzer Admin <admin@jeyzer.org>
2020-04-06 08:01:28 +0200

    Title changed

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
 			<div className="App">
 			  <header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
-				<h1 className="App-title">Welcome to React</h1>
+				<h1 className="App-title">Fib Calculator</h1>
 				<Link to="/">Hello</Link>
 				<Link to="/otherpage">Other page</Link>
 			  </header>
